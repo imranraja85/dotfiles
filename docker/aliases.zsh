40446cdeaf50748ffc5337c727e71e628d1c9290
Imran Raja <imran.raja@ge.com>
2016-09-19 10:00:59 -0400

    added docker config

---
+++ docker/aliases.zsh
@@ -0,0 +1,9 @@
+alias dm="docker-machine"
+alias d="docker"
+alias docker_remove_exited='docker rm -v `docker ps -a -q -f status=exited`'
+alias docker_remove_dangling_containers='docker rmi -f $(docker images -q -a -f dangling=true)'
+
+# Switch to another docker-machine environment
+dmeval() {
+  eval "$(docker-machine env $1)"
+}
