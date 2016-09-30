98888268f0e6243fd43329e2e394d8c705706638
Imran Raja <imran.raja@ge.com>
2016-09-30 09:18:47 -0400

    preserve path when evaluating a ucp bundle

--- docker/prompt.zsh
+++ docker/prompt.zsh
@@ -1,6 +1,8 @@
 # configure your shell to use a UCP bundle
 ub() {
+  current_dir=$(pwd)
   cd ~/.ssh/ucp_bundles/$1 && eval $(<env.sh)
+  cd $current_dir
 }
 
 # Switch to another docker-machine environment
