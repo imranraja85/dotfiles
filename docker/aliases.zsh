0250102a384168eef05416c981084f8e47445bca
Imran Raja <imranraja85@gmail.com>
2016-12-23 15:25:56 -0500

    add docker-compose alias

--- docker/aliases.zsh
+++ docker/aliases.zsh
@@ -1,5 +1,6 @@
 alias dm="docker-machine"
 alias d="docker"
+alias dc="docker-compose"
 alias di="docker info"
 alias docker_remove_exited='docker rm -v `docker ps -a -q -f status=exited`'
 alias docker_remove_dangling_containers='docker rmi -f $(docker images -q -a -f dangling=true)'
