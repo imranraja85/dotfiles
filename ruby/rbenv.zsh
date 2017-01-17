682af13f9d7eaf4670e12d2012003c2eb7727129
Imran Raja <imran.raja@ge.com>
2017-01-17 12:35:18 -0500

    slow term fix

--- ruby/rbenv.zsh
+++ ruby/rbenv.zsh
@@ -1,5 +1,7 @@
-# init according to man page
-if (( $+commands[rbenv] ))
-then
-  eval "$(rbenv init -)"
-fi
+# causes my terminal to be super slow when opening ruby files
+
+## init according to man page
+#if (( $+commands[rbenv] ))
+#then
+#  eval "$(rbenv init -)"
+#fi
